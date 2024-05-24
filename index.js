a24104ecb13389b82ffcc431d973b08ab9326c23
Daniil191245 <131523169+Daniil191245@users.noreply.github.com>
2024-05-24 20:38:33 +0300

    homework

--- index.js
+++ index.js
@@ -60,3 +60,105 @@ alert(
 // в принципі, як ще варіант, можна записати через розгалуження
 
 // Створити змінні для зберігання цілочисельного значення та рядка з числом у вигляді рядка. Використати метод parseInt() для перетворення рядка у ціле число та вивести результат в консоль. Потім використати метод toString() для перетворення цілочисельного значення у рядок та вивести результат в консоль.
+
+// Додаткові завдання
+// Створіть змінні a1, a2, a3, a4, a5 . За допомогою математичних операторів (додавання, віднімання і т.д.) знайдіть значення виразів: 5 + 3; 5 - 3; 5 * 3; 5/3, помістивши результат кожного виразу в змінну.
+
+const a1 = 5 + 3;
+const a2 = 5 - 3;
+const a3 = 5 * 3;
+const a4 = 5 / 3;
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+
+// Створіть змінні a6, a7, a8, a9, a10. Помістіть в них результат виразів: 5 % 3; 3 % 5; 5 + '3'; '5' - 3; 75 + 'кг' і виведіть на екран за допомогою alert по черзі всі змінні.
+let a6 = 5 % 3;
+let a7 = 3 % 5;
+let a8 = 5 + "3";
+let a9 = "5" - 3;
+let a10 = 75 + "кг";
+alert(a6);
+alert(a7);
+alert(a8);
+alert(a9);
+alert(a10);
+
+// Напишіть скрипт для створення випадкового числа з плаваючою крапкою від min до max (не враховуючи max).
+const min = 0.8;
+const max = 3.2;
+const randomNumber = Math.floor(Math.random() * (max - min)) + min;
+console.log(randomNumber);
+
+// Створіть функцію яка генерує випадкове ціле число від min до max включно.
+// це є аналогічне завдання до попереднього просто з цілим числом, але алгоритм той самий.
+const minWhole = 81;
+const maxWhole = 163;
+const randomWhole =
+  Math.floor(Math.random() * (maxWhole - minWhole)) + minWhole;
+console.log(randomWhole);
+
+// Напишіть скрипт, який просить відвідувача ввести два числа, і в результаті показує їх суму.
+const numberOne = prompt("Введіть число");
+const numberTwo = prompt("Введіть будь-яке число");
+const num1 = parseFloat(numberOne);
+const num2 = parseFloat(numberTwo);
+const numberGeneral = num1 + num2;
+alert("сума становить" + numberGeneral);
+
+// Творче завдання
+
+// Створити змінні к1… к4 , в них почергово записати ТІЛЬКИ результат приведення до ЦІЛОГО ЧИСЛА з даних : 1) 5px; 2) 12djd334; 3) 12.45asdwe12; 4) qwqweeewq;
+const k1 = "5px";
+const k2 = "12djd334";
+const k3 = "12.45asdwe12";
+const k4 = "qwqweeewq";
+// в цьому випадку ми беремо parseInt так як воно виділяє тільки ціле число, що й насм треба в цій задачі.
+console.log(Number.parseInt(k1));
+console.log(Number.parseInt(k2));
+console.log(Number.parseInt(k3));
+console.log(Number.parseInt(k4));
+
+// Створити змінні к1… к4 , в них почергово записати ТІЛЬКИ результат приведення до ДРОБОВОГО ЧИСЛА з даних : 1) 5px; 2) 12djd334; 3) 12.45asdwe12; 4) qwqweeewq;
+const kOne = "5px";
+const kTwo = "12djd334";
+const kThree = "12.45asdwe12";
+const kFoure = "qwqweeewq";
+console.log(Number.parseFloat(kOne));
+console.log(Number.parseFloat(kTwo));
+console.log(Number.parseFloat(kThree));
+console.log(Number.parseFloat(kFoure));
+
+// За допомогою об’єкту Math виведіть в консоль найбільше і найменше значення серед заданих (2, 34, 99, 3, 22, 36, 733, 18)
+console.log(Math.max(2, 34, 99, 3, 22, 36, 733, 18));
+console.log(Math.min(2, 34, 99, 3, 22, 36, 733, 18));
+
+// У змінну random згенеруйте випадкове число від 3 до 19 включно. Результат виведіть в консоль. 
+// (Треба ще подумати як виконати це завдання)
+
+// Супер творче завдання
+// Напишіть скрипт, який знаходить площу прямокутника. Висота 23см, шириною 10см — всі данні повинні зберігатися в змінних. Значення площі повинно зберігається в числовій змінній.
+const height = 23;
+const width = 10;
+const area = height * width;
+console.log(area);
+
+// Напишіть скрипт, який знаходить обсяг циліндра висотою 10м і діаметром основи 4м , результат та дані повинні зберігатися в змінних.
+const cylinderHeight = 10;
+const cylinderDiameter = 4;
+// почнемо з того на що нам треба орієнтуватися це формула V = πR2H
+// Math.pow() метод об'єкту Math повертає число n в степінь step, nstep.
+const cylinderRadius = cylinderDiameter / 2;
+const cylinderRadius2 = Math.pow(cylinderRadius, 2);
+const cylinderVolume = Math.PI * cylinderRadius2 * cylinderHeight;
+console.log(cylinderVolume); //125.66370614359172
+// в результаті чого виводить не гарне число, спробуємо його зробити гарніше.
+console.log(Math.round(cylinderVolume)); // 126
+
+// У прямокутного трикутника дві сторони зі значенням 3 і 4 Знайдіть гіпотенузу k по теоремі Піфагора (потрібно використовувати функцію Math.pow (число, ступінь) або оператор піднесення до степеня **). Результат та дані повинні зберігатися в змінних.
+const katetOne = 4;
+const katetTwo = 3;
+const sumaKatet = Math.pow(katetOne, 2) + Math.pow(katetTwo, 2);
+const hypotenuse = Math.sqrt(sumaKatet);
+console.log(hypotenuse);
