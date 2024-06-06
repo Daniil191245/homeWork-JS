93eb6c4d8191fb59a666649ba7774efc4c0387f8
Daniil191245 <131523169+Daniil191245@users.noreply.github.com>
2024-06-06 13:47:44 +0300

    homework

--- home.js
+++ home.js
@@ -1,1 +1,19 @@
-
+// 1)
+const button = document.querySelector(".button");
+const input = document.querySelector(".input");
+button.textContent = input.value;
+console.log(button);
+// 2)
+const img = document.querySelector(".img");
+img.src =
+  "https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?cs=srgb&dl=pexels-souvenirpixels-417074.jpg&fm=jpg";
+// 3)
+const link = document.querySelector(".link");
+link.href = "https://rozetka.com.ua/ua/";
+const img1 = document.querySelector(".img1");
+img1.alt = "nice nature";
+// 4)
+const oneListElements = document.querySelector(".list");
+const oneListElementChild =
+  oneListElements.firstElementChild.querySelector(".text");
+oneListElementChild.textContent = "Hello one elements";
