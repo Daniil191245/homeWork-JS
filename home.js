f8f6a3f8530f3fe53f8ab42e4adb9535825ffb59
Daniil191245 <131523169+Daniil191245@users.noreply.github.com>
2024-06-12 17:41:28 +0300

    feat: the finish of the modal of the first task

--- home.js
+++ home.js
@@ -1,1 +1,8 @@
-
+const openButton = document.querySelector("[data-action = open-modal]");
+const closeButton = document.querySelector("[data-action = close-modal]");
+const modalka = document.querySelector(".backdrop");
+openButton.addEventListener("click", togModal);
+closeButton.addEventListener("click", togModal);
+function togModal() {
+  document.body.classList.toggle("show-modal");
+}
