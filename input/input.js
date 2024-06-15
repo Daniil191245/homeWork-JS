6e7945695496e39ab35f872d93a4f561e665fbc7
Daniil191245 <131523169+Daniil191245@users.noreply.github.com>
2024-06-15 21:33:37 +0300

    feat: update homework inputs

--- input/input.js
+++ input/input.js
@@ -5,3 +5,14 @@ textSize.addEventListener("input", fontSizeText);
 function fontSizeText() {
   text.style.fontSize = textSize.value + "px";
 }
+// 4
+const input = document.getElementById("name-input");
+const span = document.getElementById("name-output");
+input.addEventListener("input", onInput);
+function onInput(e) {
+  const inputValue = e.target.value;
+  span.textContent = inputValue;
+  if (inputValue.trim().length === 0) {
+    span.textContent = "незнайомець";
+  }
+}
